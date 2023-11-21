97cc2e954bfb561ea0a368440f916dcf0ac829fc
Ayoung <thay123028@naver.com>
2023-11-22 05:31:17 +0900

    5000

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app){
     app.use(
         "/user",
       createProxyMiddleware( {
-        target: 'http://3.112.14.157:80',
+        target: 'http://3.112.14.157:5000',
         changeOrigin: true,
       })
     )
